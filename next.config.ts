93459f71dde0b6d25432e9a21cd4965a28c9731b
seandurcan <seandurcan@gmail.com>
2026-09-15 10:44:58 +0100

    Send global X-Robots-Tag noindex header

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "10.187.172.250",
   ],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow, noarchive, nosnippet, noimageindex",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
