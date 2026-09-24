89413ae040f953d85286ab87558d32de31a83eb2
seandurcan <seandurcan@gmail.com>
2026-09-24 15:15:12 +0100

    Make multi-competition migration data-safe

--- prisma/migrations/20260924140500_allow_multiple_competitions_per_year/migration.sql
+++ prisma/migrations/20260924140500_allow_multiple_competitions_per_year/migration.sql
@@ -1,3 +1,2 @@
 -- Allow multiple competitions to share a calendar/season start year.
 DROP INDEX IF EXISTS "Tournament_year_key";
-CREATE UNIQUE INDEX "Tournament_name_year_key" ON "Tournament"("name", "year");
